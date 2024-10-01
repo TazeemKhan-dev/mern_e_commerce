402ea06f6f7600515154eccd19815a9d5ea6fa7d
Tazeem khan <tazeemkhan0712@gmail.com>
2024-10-01 22:06:57 +0530

    completed

--- backend/index.js
+++ backend/index.js
@@ -32,6 +32,6 @@ app.use("/orders", orderRouter);
 app.use("/carts", cartRouter);
 app.use("/checkout", stripeRoute);
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("Server running on port 5000");
 });
